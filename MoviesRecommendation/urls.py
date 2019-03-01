45a10a32315cb65128e785bd346b80aeec0f4b7a
187107363 <43198174+187107363@users.noreply.github.com>
2019-03-01 14:41:55 +0800

    Update urls.py

--- MoviesRecommendation/urls.py
+++ MoviesRecommendation/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/', include('MoviesRecommendation.Recommend.urls')),
+    url(r'^movies/', include('MoviesRecommendation.Recommend.urls')),
 ]
