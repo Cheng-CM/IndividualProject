d008ba4c0022203855004b9bfba2c4c637a2a640
Cheng ChakMan <chengchakman@gmail.com>
2019-03-04 15:51:19 +0800

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// app.js
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import axios from 'axios'
 import VueAxios from 'vue-axios'
