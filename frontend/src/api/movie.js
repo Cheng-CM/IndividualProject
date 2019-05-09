9a9cb0980c741ab89ffb0a75802c344aa7cfc9cf
187107363 <43198174+187107363@users.noreply.github.com>
2019-05-10 00:41:30 +0800

    Update movie.js

--- frontend/src/api/movie.js
+++ frontend/src/api/movie.js
@@ -1,27 +1,30 @@
 import Vue from 'vue';
 class MovieAPI {
-    getMovie(params){
+    getMovie(params) {
         return Vue.prototype.axios.get("http://localhost:8000/api/m/" + params);
-    } 
-    getRandomMovie(){
+    }
+    getRandomMovie() {
         return Vue.prototype.axios.get("http://localhost:8000/api/rm/");
-    } 
-    postsRate(params){
+    }
+    postsRate(params) {
         return Vue.prototype.axios.post("http://localhost:8000/api/sr/?", params);
     }
-    postcRate(params){
+    postcRate(params) {
         return Vue.prototype.axios.post("http://localhost:8000/api/cr/?", params);
     }
-    getgtUserid(){
+    getgtUserid() {
         return Vue.prototype.axios.get("http://localhost:8000/api/gcr/");
-    } 
-    getLink(params){
+    }
+    getLink(params) {
         return Vue.prototype.axios.get("http://localhost:8000/api/l/" + params);
-    } 
-    getMovieImage(params){
-        return Vue.prototype.axios.get("https://api.themoviedb.org/3/movie/"+ params +"/images?api_key=34132d035e63f344a5eb1a1ac50e64e2&language=en");
-    } 
-    
-    
+    }
+    getMovieImage(params) {
+        return Vue.prototype.axios.get("https://api.themoviedb.org/3/movie/" + params + "/images?api_key=34132d035e63f344a5eb1a1ac50e64e2&language=en");
+    }
+    getRecommendResult(params) {
+        return Vue.prototype.axios.get("http://localhost:8000/recommend/?id=" + params);
+    }
+
+
 }
 export default new MovieAPI();
