59d3df3ac167ddc4710fd1e70a788d1b857beeeb
Cheng Chak Man <43198174+187107363@users.noreply.github.com>
2020-01-27 17:35:47 +0800

    Clean up css

--- frontend/src/light-bootstrap-main.js
+++ frontend/src/light-bootstrap-main.js
@@ -11,7 +11,7 @@ import SideBar from './components/SidebarPlugin'
 // asset imports
 import 'bootstrap/dist/css/bootstrap.css'
 import './assets/sass/light-bootstrap-dashboard.scss'
-import './assets/css/demo.css'
+import './assets/css/main.css'
 
 /**
  * This is the main Light Bootstrap Dashboard Vue plugin where dashboard related plugins are registerd.
