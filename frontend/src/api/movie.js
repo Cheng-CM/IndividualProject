a523b036d71118782808a1ce140cc4f16b2fea4b
Cheng Chak Man <43198174+187107363@users.noreply.github.com>
2020-01-27 22:07:10 +0800

    fixing gitignore

--- frontend/src/api/movie.js
+++ frontend/src/api/movie.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import api_key from 'frontend-config.yml';
 class MovieAPI {
     getMovie(params) {
         return Vue.prototype.axios.get("http://localhost:8000/api/m/" + params);
@@ -19,7 +20,7 @@ class MovieAPI {
         return Vue.prototype.axios.get("http://localhost:8000/api/l/" + params);
     }
     getMovieImage(params) {
-        return Vue.prototype.axios.get("https://api.themoviedb.org/3/movie/" + params + "/images?api_key=34132d035e63f344a5eb1a1ac50e64e2&language=en");
+        return Vue.prototype.axios.get("https://api.themoviedb.org/3/movie/" + params + "/images?api_key=",api_key,"&language=en");
     }
     getRecommendResult(params) {
         return Vue.prototype.axios.get("http://localhost:8000/recommend/?id=" + params);
