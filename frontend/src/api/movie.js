b104c8114596839c17b262a84265694c46cf6e5c
Cheng Chak Man <43198174+187107363@users.noreply.github.com>
2020-01-28 16:08:22 +0800

    renew api key and add config

--- frontend/src/api/movie.js
+++ frontend/src/api/movie.js
@@ -1,38 +1,38 @@
 import Vue from 'vue';
-import api_key from 'frontend-config.yml';
+import config from "../config.json";
 class MovieAPI {
     getMovie(params) {
-        return Vue.prototype.axios.get("http://localhost:8000/api/m/" + params);
+        return Vue.prototype.axios.get("http://"+ config.development.database +":"+config.development.db_port+"/api/m/" + params);
     }
     getRandomMovie() {
-        return Vue.prototype.axios.get("http://localhost:8000/api/rm/");
+        return Vue.prototype.axios.get("http://"+ config.development.database +":"+config.development.db_port+"/api/rm/");
     }
     postsRate(params) {
-        return Vue.prototype.axios.post("http://localhost:8000/api/sr/?", params);
+        return Vue.prototype.axios.post("http://"+ config.development.database +":"+config.development.db_port+"/api/sr/?", params);
     }
     postcRate(params) {
-        return Vue.prototype.axios.post("http://localhost:8000/api/cr/?", params);
+        return Vue.prototype.axios.post("http://"+ config.development.database +":"+config.development.db_port+"/api/cr/?", params);
     }
     getgtUserid() {
-        return Vue.prototype.axios.get("http://localhost:8000/api/gcr/");
+        return Vue.prototype.axios.get("http://"+ config.development.database +":"+config.development.db_port+"/api/gcr/");
     }
     getLink(params) {
-        return Vue.prototype.axios.get("http://localhost:8000/api/l/" + params);
+        return Vue.prototype.axios.get("http://"+ config.development.database +":"+config.development.db_port+"/api/l/" + params);
     }
     getMovieImage(params) {
-        return Vue.prototype.axios.get("https://api.themoviedb.org/3/movie/" + params + "/images?api_key=",api_key,"&language=en");
+        return Vue.prototype.axios.get("https://api.themoviedb.org/3/movie/" + params + "/images?api_key=" + config.development.api_key + "&language=en");
     }
     getRecommendResult(params) {
-        return Vue.prototype.axios.get("http://localhost:8000/recommend/?id=" + params);
+        return Vue.prototype.axios.get("http://"+ config.development.database +":"+config.development.db_port+"/recommend/?id=" + params);
     }
     postResult(params) {
-        return Vue.prototype.axios.post("http://localhost:8000/result", params);
+        return Vue.prototype.axios.post("http://"+ config.development.database +":"+config.development.db_port+"/result", params);
     }
     getAccuracy() {
-        return Vue.prototype.axios.get("http://localhost:8000/getAccuracy");
+        return Vue.prototype.axios.get("http://"+ config.development.database +":"+config.development.db_port+"/getAccuracy");
     }
     search(params) {
-        return Vue.prototype.axios.get("http://localhost:8000/search/?name=" + params);
+        return Vue.prototype.axios.get("http://"+ config.development.database +":"+config.development.db_port+"/search/?name=" + params);
     }
 
 
